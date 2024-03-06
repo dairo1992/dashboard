04793328a4895c53bd4cacb1625d0866997220c6
Dairo Barrios <dairo.barrios.ramos@gmail.com>
2024-03-06 16:58:34 -0500

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 import { TopbarComponent } from './shared/topbar/topbar.component';
+import { SidenavComponent } from './shared/sidenav/sidenav.component';
+import { AppMenuitemComponent } from './shared/app.menuitem.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopbarComponent],
+  imports: [
+    RouterOutlet,
+    TopbarComponent,
+    SidenavComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
