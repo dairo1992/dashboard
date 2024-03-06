799e1ecd5a454ce6a15c3b7f1f169e54546650e8
Dairo Barrios <dairo.barrios.ramos@gmail.com>
2024-03-06 14:55:13 -0500

    antes de implementar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { FormControl, FormGroup } from '@angular/forms';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, SelectButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  formGroup!: FormGroup;
+  sw: boolean = true;
+  stateOptions: any[] = [
+    { label: 'Off', value: 'off' },
+    { label: 'On', value: 'on' },
+  ];
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.formGroup = new FormGroup({
+      value: new FormControl('on'),
+    });
+  }
 }
